af7d61aa6002beab7daa49da0d37a65a5fd1a1c9
Rafael Gomes <gorraffa@hotmail.com>
2025-07-09 14:54:38 -0300

    H2 database

---
+++ backend/src/main/java/br/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package br.com.devsuperior.dscatalog.repositories;
+
+import br.com.devsuperior.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
